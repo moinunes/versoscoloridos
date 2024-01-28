4968c71efcdbd1b3ff57d3767c6caeadbe082921
Moises Nunes <moinunes@gmail.com>
2024-01-28 08:39:33 -0300

    melhorias 2

--- js/mens.js
+++ js/mens.js
@@ -1,24 +1,23 @@
 /*-------------------------------------------------------------
-* obter a  mensagem. verso, frase  com base no segundo fornecido
+* obter a  mensagem de forma cíclica
 *-------------------------------------------------------------*/ 
 function obterMens() {
-   var frases1 = [  
-   ];
+   
    var frases = [
-      "🌟 Deus, põe teu olho amoroso sobre todos os que já tiveram um amor sem nojo nem medo, e de alguma forma insana esperam a volta dele: que os telefones toquem, que as cartas finalmente cheguem … Sobre todos aqueles que ainda continuam tentando, Deus, derrama teu Sol mais luminoso. \n - Caio Fernando Abreu - ",
-      "😊 Não tem poesia nem palavra difícil e nem construção sofisticada. O amor é simples como sorrir numa droga de fila. E não se sentir mais sozinho e nem esperando e nem desesperado e nem morrendo e nem com tanto medo. \n - Tati Bernardi -",
-      "📜 Os poetas mentiram pra mim, Roberto Carlos mentiu para a gente. O amor não é manso assim. Ele pega, invade e devora a gente. \n - Caio Fernando Abreu",
-      "🌙 Lua na lagoa, \n rãs cantam canções noturnas,\n serenata azul.",
-      "🌄 Neblina da manhã,\n teias de aranha cintilam, \n sol beija a montanha.",
-      "🌸 Cerejeiras choram, \n pétalas dançam no vento, \n primavera sonha.",
-      "✍️ Fazer poesia é como fazer amor: nunca se saberá se a própria alegria é compartilhada.\n  - Cesare Pavese ",
-      "🤔 Enquanto houver um louco, um poeta e um amante haverá sonho, amor e fantasia. E enquanto houver sonho, amor e fantasia, haverá esperança. \n -  William Shakespeare  ",
-      "💗 Todos os dias, quando acordo, vou correndo tirar a poeira da palavra amor... \n - Clarice Lispector ",
-      "💑 Ao toque do amor, todo homem se torna um poeta. - Platão - ",
-      "🤔 Talvez os poetas estejam certos. Talvez o amor seja a única resposta \n - Woody Allen ",
-      "📜 O amor é a poesia dos sentidos. Ou é sublime, ou não existe. Quando existe, existe para sempre e vai crescendo dia a dia. \n - Honoré de Balzac ",
-      "📜 tudo dito, \n nada feito, \n fito e deito \n  - Paulo Leminski -",
-      "📜 Viver é super difícil \n  no mais fundo \n está sempre na superfície \n - Paulo Leminski ",
+      "🌟 Deus, põe teu olho amoroso sobre todos os que já tiveram um amor sem nojo nem medo, e de alguma forma insana esperam a volta dele: que os telefones toquem, que as cartas finalmente cheguem … Sobre todos aqueles que ainda continuam tentando, Deus, derrama teu Sol mais luminoso. <br><span class='small'>- Caio Fernando Abreu</span>",
+      "😊 Não tem poesia nem palavra difícil e nem construção sofisticada. O amor é simples como sorrir numa droga de fila. E não se sentir mais sozinho e nem esperando e nem desesperado e nem morrendo e nem com tanto medo. <br><span class='small'>- Tati Bernardi</span>",
+      "📜 Os poetas mentiram pra mim, Roberto Carlos mentiu para a gente. O amor não é manso assim. Ele pega, invade e devora a gente. <br><span class='small'>- Caio Fernando Abreu</span>",
+      "🌙 Lua na lagoa, <br> rãs cantam canções noturnas,<br> serenata azul.",
+      "🌄 Neblina da manhã,<br> teias de aranha cintilam, <br> sol beija a montanha.",
+      "🌸 Cerejeiras choram, <br> pétalas dançam no vento, <br> primavera sonha.",
+      "✍️ Fazer poesia é como fazer amor: nunca se saberá se a própria alegria é compartilhada. <br><span class='small'>- Cesare Pavese</span>",
+      "🤔 Enquanto houver um louco, um poeta e um amante haverá sonho, amor e fantasia. E enquanto houver sonho, amor e fantasia, haverá esperança. <br><span class='small'>- William Shakespeare</span>",
+      "💗 Todos os dias, quando acordo, vou correndo tirar a poeira da palavra amor... <br><span class='small'>- Clarice Lispector</span>", 
+      "💑 Ao toque do amor, todo homem se torna um poeta. <br><span class='small'>- Platão</span>",
+      "🤔 Talvez os poetas estejam certos. Talvez o amor seja a única resposta <br><span class='small'>- Woody Allen</span>",
+      "📜 O amor é a poesia dos sentidos. Ou é sublime, ou não existe. Quando existe, existe para sempre e vai crescendo dia a dia. <br><span class='small'>- Honoré de Balzac</span>",
+      "📜 tudo dito, <br> nada feito, <br> fito e deito <br><span class='small'>- Paulo Leminski</span>",
+      "📜 Viver é super difícil <br>  no mais fundo <br> está sempre na superfície <br><span class='small'>- Paulo Leminski</span>",
       "🌬️ Nas asas do vento, dança a melodia dos sonhos perdidos.",
       "💗 No crepúsculo dourado, o silêncio revela segredos do coração.",
       "✨ Estrelas sussurram versos de amor no céu noturno.",
@@ -34,24 +33,29 @@ function obterMens() {
       "🎶 Cada batida do meu coração sussurra o teu nome, uma canção de amor eterno.",
       "😊 Teu sorriso é a luz do sol, aquecendo minha alma com ternura infinita.",
       "💞 Nossas almas dançam juntas, entrelaçadas na coreografia eterna do amor.",
-      "🌌 Sereno riacho, \n sob a lua, dança a brisa, \n noite sussurra paz.",
-      "🍂 Pétalas caem, \n primavera em despedida, \n silêncio floresce.",
-      "☀️ Sol se põe no mar, \n gaivotas dançam no vento, \n horizonte abraça.",
-      "🌲 No bosque tranquilo, \n sombras dançam com a luz, \n segredos da floresta.",
-      "💧 Gota na folha, \n a natureza respira, \n vida em equilíbrio.",
-      "📜 Não há ninguém, mesmo sem cultura, que não se torne poeta quando o amor toma conta dele. - Platão."
-      // ... adicione mais frases conforme necessário
-    ];
+      "🌌 Sereno riacho, <br> sob a lua, dança a brisa, <br> noite sussurra paz.",
+      "🍂 Pétalas caem, <br> primavera em despedida, <br> silêncio floresce.",
+      "☀️ Sol se põe no mar, <br> gaivotas dançam no vento, <br> horizonte abraça.",
+      "🌲 No bosque tranquilo, <br> sombras dançam com a luz, <br> segredos da floresta.",
+      "💧 Gota na folha, <br> a natureza respira, <br> vida em equilíbrio.",
+      "📜 Não há ninguém, mesmo sem cultura, que não se torne poeta quando o amor toma conta dele. <br><span class='small'>- Platão</span>",
+   ];
+     
+       frases1 = [
+         "🤔 Enquanto houver um louco, um poeta e um amante haverá sonho, amor e fantasia. E enquanto houver sonho, amor e fantasia, haverá esperança. <br><span class='small'>- William Shakespeare</span>",
+   
+      ];
+       
+      // <br><span class='small'>- xxxxxxxxx</span>",
+     
     
 
-   var frase_padrao = "Sua autenticidade é sua beleza.\n Abrace quem você é, pois você é único e especial.";
-
+   var frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+ 
    // Obter o segundo atual
    var segundoAtual = new Date().getSeconds();
-
    // Calcula o índice de forma cíclica
-   var indiceFrase = segundoAtual % frases.length;
-   
+   var indiceFrase = segundoAtual % frases.length;   
    // Obtém a frase correspondente ao índice calculado
    var fraseCorrespondente = frases[indiceFrase] || frase_padrao;
 
--- js/scripts.js
+++ js/scripts.js
@@ -74,16 +74,16 @@ function compartilharWhatsApp(id) {
        var linkPoema = poemaDiv.prev('.clicavel').text().trim();
        var autorTitulo = poemaDiv.find('.font_poema_titulo').text().replace('Autor:', '').trim();
        var poemaConteudo = poemaDiv.find('.font_poema_1').clone(); 
-       var poemaTexto = poemaConteudo.html().replace(/<br>\s*<br>/g, '<br>\n').replace(/<br>$/g, '');
-       var linhas = poemaTexto.split('\n');
+       var poemaTexto = poemaConteudo.html().replace(/<br>\s*<br>/g, '<br><br>').replace(/<br>$/g, '');
+       var linhas = poemaTexto.split('<br>');
        for (var i = 0; i < linhas.length; i++) {
          linhas[i] = linhas[i].trim();
        }
-       var poemaFormatado = linhas.join('\n') + '\n';
+       var poemaFormatado = linhas.join('<br>') + '<br>';
        var mensagemSpan = poemaDiv.find('.mensagem');
        poemaFormatado = poemaFormatado.replace(/<br>/g, '');
        var urlPagina = window.location.href;
-       var conteudoCompleto = linkPoema + '\nAutor: ' + autorTitulo + poemaFormatado + 'Poemas: ' + urlPagina;
+       var conteudoCompleto = linkPoema + '<br>Autor: ' + autorTitulo + poemaFormatado + 'Poemas: ' + urlPagina;
        var $tempTextArea = $('<textarea>');
        $tempTextArea.val(conteudoCompleto);
        $('body').append($tempTextArea); 
@@ -110,14 +110,14 @@ function compartilharWhatsApp(id) {
 $(document).ready(function () {
    $('.btn_copiar_verso').on('click', function () {
        var blocoTexto = $(this).closest('.card-body').find('.card-text');
-       var linhas = blocoTexto.text().trim().split('\n');
+       var linhas = blocoTexto.text().trim().split('<br>');
        var urlPagina = window.location.href;
        for (var i = 0; i < linhas.length; i++) {
            linhas[i] = linhas[i].trimLeft(); // Use trimLeft para remover espaços à esquerda
        }
-       var textoFormatado = linhas.join('\n');
+       var textoFormatado = linhas.join('<br>');
        var tempInput = $('<textarea>');
-       tempInput.val(textoFormatado+ '\n\n' + urlPagina);
+       tempInput.val(textoFormatado+ '<br><br>' + urlPagina);
        $('body').append(tempInput);
        tempInput.select();
        document.execCommand('copy');
@@ -130,27 +130,27 @@ $(document).ready(function () {
 
  
 /*-------------------------------------------------------------
-* obter a frase com base no segundo fornecido
+* obter a frase de forma cíclica
 *-------------------------------------------------------------*/ 
 function obterFrase() {
-   // Obter o segundo atual
-   var segundoAtual = new Date().getSeconds();
-   // Formatando o segundo para duas casas decimais
-   var segundoFormatado = ("0" + segundoAtual).slice(-2);
-
    var frases = [
+      "🦸‍♂️ Um herói é alguém que caminha voluntariamente para o desconhecido. <br><span class='small'>- Tom Hanks</span>",
+      "🌟 Nunca deixe que lhe digam que não vale a pena  acreditar no sonho que se tem ou que seus planos nunca vão dar certo <br> ou que você nunca vai ser alguém... <br><span class='small'>- Renato Russo</span>", 
+      "🌌 Ninguém além de nós mesmos pode libertar nossa mente. <br> <span class='small'>- Bob Marley</span>",
+      "❤️ O sucesso de cada mulher deveria ser uma inspiração para outra mulher. <br> Somos mais fortes quando nos apoiamos.   <br> <span class='small'>- Serena Williams</span>",
+      "🌅 Quem olha para fora sonha, quem olha para dentro desperta. <br> <span class='small'>- Carl Jung</span>",
       "🌟 Acredite em você mesmo e tudo será possível.",
       "🚀 O que você faz hoje pode semear um futuro melhor amanhã.",
       "🕰️ Nunca é tarde demais para ser quem você poderia ter sido.",
       "🔍 O sucesso é a soma de pequenos esforços repetidos dia após dia.",
       "🌍 Seja a mudança que você deseja ver no mundo.",
       "🎉 A vida é feita de escolhas, faça escolhas que te façam feliz.",
-      "🗝️ O sucesso não é a chave para a felicidade.\n A felicidade é a chave para o sucesso.",
-      "🌠 Nunca desista do que você realmente quer fazer.\n A pessoa com grandes sonhos é mais poderosa do que aquela com todos os fatos.",
+      "🗝️ O sucesso não é a chave para a felicidade.<br> A felicidade é a chave para o sucesso.",
+      "🌠 Nunca desista do que você realmente quer fazer.<br> A pessoa com grandes sonhos é mais poderosa do que aquela com todos os fatos.",
       "❤️ A única maneira de fazer um ótimo trabalho é amar o que você faz.",
       "🌈 A vida reserva surpresas maravilhosas para todos aqueles que cultivam a gratidão!",
-      "🌳 O melhor momento para plantar uma árvore foi há 20 anos.\n O segundo melhor momento é agora.",
-      "🧠 A mente é tudo.\n Você se torna aquilo que você pensa.",
+      "🌳 O melhor momento para plantar uma árvore foi há 20 anos.<br> O segundo melhor momento é agora.",
+      "🧠 A mente é tudo.<br> Você se torna aquilo que você pensa.",
       "📚 O único lugar onde o sucesso vem antes do trabalho é no dicionário.",
       "🙏 A gratidão transforma o que temos em suficiente.",
       "🌈 Seja o arco-íris na nuvem de alguém.",
@@ -159,12 +159,12 @@ function obterFrase() {
       "🚶 A persistência é o caminho do êxito.",
       "🌞 O otimismo é a fé em ação.",
       "🌌 Quem cultiva gratidão é capaz de realizar sonhos que parecem inalcançáveis!",
-      "🌟 A verdadeira medida do sucesso não reside nas vitórias fáceis, \n mas sim em quantas vezes você consegue se recuperar do fracasso. \n Cada revés é uma oportunidade de crescimento e superação.",
+      "🌟 A verdadeira medida do sucesso não reside nas vitórias fáceis, <br> mas sim em quantas vezes você consegue se recuperar do fracasso. <br> Cada revés é uma oportunidade de crescimento e superação.",
       "🍀 A verdadeira sorte consiste em ter algo pelo que lutar.",
       "🛠️ Não há substituto para o trabalho árduo.",
       "🌎 A vida é 10% do que acontece conosco e 90% como reagimos a isso.",
       "❤️ O único modo de fazer um excelente trabalho é amar o que você faz.",
-      "🌅 Sua vida só melhora quando você faz.\n Trabalhe em si mesmo e o resto seguirá.",
+      "🌅 Sua vida só melhora quando você faz.<br> Trabalhe em si mesmo e o resto seguirá.",
       "🚀 A felicidade é uma jornada, não um destino.", 
       "😊 Seja você mesmo; todos os outros já estão ocupados.",
       "🏃 Não importa o quão devagar você vá, desde que você não pare.",
@@ -173,7 +173,7 @@ function obterFrase() {
       "🚶‍♂️ O sucesso é ir de fracasso em fracasso sem perder o entusiasmo.",
       "❤️ A única maneira de fazer um excelente trabalho é amar o que você faz.",
       "👣 Se você está indo pela estrada certa e está disposto a continuar andando, eventualmente você chegará lá.",
-      "🔓 A única limitação que você possui é aquela que você escolhe impor a si mesmo.\n Liberte-se das barreiras autoimpostas e abra caminho para suas verdadeiras capacidades.",
+      "🔓 A única limitação que você possui é aquela que você escolhe impor a si mesmo.<br> Liberte-se das barreiras autoimpostas e abra caminho para suas verdadeiras capacidades.",
       "🚫 Não há atalhos para qualquer lugar que valha a pena.",
       "🎉 Quanto mais você se elogia e celebra sua vida, mais há na vida para celebrar.",
       "🚀 A chave para o sucesso é começar antes de estar pronto.",
@@ -183,32 +183,46 @@ function obterFrase() {
       "🌎 A vida é 10% do que acontece conosco e 90% como reagimos a isso.",
       "🌠 A única maneira de alcançar o impossível é acreditar que é possível.", 
       "🔄 Sua vida não melhora apenas por esperar.", 
-      "🌱 A felicidade não é algo pronto. \n Ela vem de suas próprias ações.",
+      "🌱 A felicidade não é algo pronto. <br> Ela vem de suas próprias ações.",
       "🚤 Você não pode atravessar o mar simplesmente parando e olhando para a água.",
       "🧂 O fracasso é o condimento que dá sabor ao sucesso.",
-      "💪 A diferença entre uma pessoa bem-sucedida e outras não é falta de força,\n não é falta de conhecimento, mas sim falta de vontade.",
-      "🚀 Não seja uma versão mais fraca de você mesmo. \n Seja uma versão mais forte de você mesmo.",
+      "💪 A diferença entre uma pessoa bem-sucedida e outras não é falta de força,<br> não é falta de conhecimento, mas sim falta de vontade.",
+      "🚀 Não seja uma versão mais fraca de você mesmo. <br> Seja uma versão mais forte de você mesmo.",
       "🌟 Você nunca é velho demais para definir outro objetivo ou sonhar um novo sonho.",
       "🚀 O sucesso é a soma de pequenos esforços repetidos dia após dia.",
       "🌅 O que você faz hoje pode melhorar todos os amanhãs.", 
       "🚀 O sucesso é a soma de pequenos esforços repetidos dia após dia.",
       "🚀 A persistência, aliada à determinação, é a fórmula mágica que transforma o impossível em realizações extraordinárias.", 
       "🎉 A vida é feita de escolhas, faça escolhas que te façam feliz.",
-      "🚀 O sucesso não é a chave para a felicidade.\n A felicidade é a chave para o sucesso.",
-      "🚀 Nunca desista do que você realmente quer fazer.\n A pessoa com grandes sonhos é mais poderosa do que aquela com todos os fatos.",
+      "🚀 O sucesso não é a chave para a felicidade.<br> A felicidade é a chave para o sucesso.",
+      "🚀 Nunca desista do que você realmente quer fazer.<br> A pessoa com grandes sonhos é mais poderosa do que aquela com todos os fatos.",
       "❤️ A única maneira de fazer um ótimo trabalho é amar o que você faz.",
-      "➡️ Acreditar que você pode é o primeiro passo para alcançar seus objetivos. \n A confiança em si mesmo é metade do caminho para o sucesso.",
-      "➡️ Não deixe que o ruído da opinião alheia \n impeça que você escute a sua voz interior. \n - Steve Jobs",
-      "➡️ Explore livremente, copie os versos que ressoarem em seu coração e compartilhe. \n Vamos espalhar a poesia juntos!",
-      "➡️ ",
-      "➡️ ",
-      "➡️ ",
-   
+      "➡️ Acreditar que você pode é o primeiro passo para alcançar seus objetivos. <br> A confiança em si mesmo é metade do caminho para o sucesso.",
+      "➡️ Não deixe que o ruído da opinião alheia impeça que você escute a sua voz interior. <br><span class='small'>- Steve Jobs</span>",
+      "➡️ Explore livremente, copie os versos que ressoarem em seu coração e compartilhe. <br> Vamos espalhar a poesia juntos!",
+      "➡️ É necessário saber ficar sozinho, em silêncio, mergulhado em si. <br> É fundamental estar satisfeito com a própria companhia. <br><span class='small'>- Filipe Ret (cantor e compositor brasileiro)</span>",
+      "🚀 Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos. <br> <span class='small'>- Friedrich Nietzsche</span>",
+      "💧 Por vezes sentimos que aquilo que fazemos não é senão uma gota de água no mar.<br> Mas o mar seria menor se lhe faltasse uma gota. <br> <span class='small'>- Madre Teresa de Calcutá</span>",
+      "🌟 Jamais desista das pessoas que ama. Jamais desista de ser feliz. Lute sempre pelos seus sonhos. <br>Seja profundamente apaixonado pela vida. Pois a vida é um espetáculo imperdível. <br> <span class='small'>- Augusto Cury</span>",
+
+
    ];
 
-   // Obtendo a frase correspondente ao segundo
-   var frase_padrao ="Sua autenticidade é sua beleza.\n Abrace quem você é, pois você é único e especial."; 
-   var fraseCorrespondente = frases[Number(segundoFormatado) - 1] || frase_padrao ;  
+   var frases1 = [
+     
+   ];
 
+   var frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+
+   // Obter o segundo atual
+   var segundoAtual = new Date().getSeconds();
+
+   // Calcula o índice de forma cíclica
+   var indiceFrase = segundoAtual % frases.length;
+   
+   // Obtém a frase correspondente ao índice calculado
+   var fraseCorrespondente = frases[indiceFrase] || frase_padrao;
+
+   
    return fraseCorrespondente;
 }
