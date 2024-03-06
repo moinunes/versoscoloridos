d75231c06c2e38709ec48d4b7352163d91fa17a3
Moises Nunes <moinunes@gmail.com>
2024-03-06 06:21:02 -0300

    implantando adsense

--- js/frases/frases_reflexao_2.js
+++ js/frases/frases_reflexao_2.js
@@ -7,34 +7,17 @@ var mensagem_1 = [
 
 
 var mensagem_2 = [
-   "Ao virar a esquina, ela se deparou com um livro esquecido no chão. Um livro que seria sua porta de entrada para um mundo de magia e mistério 📚",
-   "A luz da lua iluminou um antigo baú no sótão, revelando segredos guardados por gerações 🌙",
-   "No fundo da gaveta, ela encontrou uma carta amarelada, escrita décadas atrás, contendo palavras que ecoavam com sua própria jornada 💌",
-   "Ao abrir a janela, ele foi saudado por uma brisa fresca carregada de promessas de aventuras desconhecidas 💨",
-   "Um bilhete perdido no bolso de sua jaqueta desencadeou uma jornada que o levaria aos confins da terra e além 🎫",
-   "Na velha caixa de recordações, ela descobriu uma fotografia antiga que capturava um momento de pura felicidade, acendendo uma chama de nostalgia e inspiração 📷",
-   "Uma trilha escondida na floresta levou-o a um lugar esquecido pelo tempo, onde segredos ancestrais aguardavam para serem revelados 🌳",
-   "A descoberta de um diário empoeirado revelou os pensamentos mais íntimos de alguém que uma vez caminhou pelos mesmos caminhos que ele agora trilhava 📔",
-   "Um bilhete de trem encontrado em um banco de parque desencadeou uma jornada que o levaria a encontrar não apenas novos destinos, mas também a si mesmo 🚂",
-   "No sótão empoeirado, ela descobriu uma caixa de música antiga, cujas melodias desencadearam memórias há muito esquecidas 🎶",
-   "Um raio de sol atravessou a cortina, iluminando uma pintura esquecida na parede, revelando uma paisagem de sonhos e aventuras 🌞",
-   "Uma simples concha encontrada na praia desencadeou uma jornada para além das ondas, onde segredos marinhos aguardavam para serem revelados 🐚",
-   "Um mapa antigo encontrado no sótão desvendou um tesouro enterrado há muito tempo, desencadeando uma busca cheia de perigos e revelações surpreendentes 🗺️",
-   "<h5 class='text-center text-info'> Portais da Descoberta: <br>Jornadas Desencadeadas por Relíquias Esquecidas 🌟</h5>",
+   "Numa pequena vila, havia um velho sábio conhecido por sua gratidão inabalável. Todos os dias, ele acordava ao amanhecer e, diante do sol nascente, expressava sua gratidão pelo dia que se iniciava.",
+   "As pessoas da vila admiravam sua atitude e perguntavam como ele mantinha tal gratidão, mesmo nos momentos difíceis. O sábio sorria e respondia que, ao focar nas bênçãos ao seu redor, mesmo as menores, ele encontrava a verdadeira felicidade.",   
+   "Sua prática diária de gratidão não apenas transformou sua própria vida, mas também inspirou os outros a adotarem uma perspectiva semelhante. Eles aprenderam que a gratidão não dependia das circunstâncias, mas sim da disposição de reconhecer e apreciar as coisas simples da vida.",
+   "<h5 class='text-center text-info'>Gratidão Infinita: O Caminho para a Felicidade 🌟</h5>",
 ];
 
 mensagem_3 = [
-   "O caminho para a sabedoria começa com a humildade de reconhecer que sempre há mais a aprender. 🌱",
-   "Nossas cicatrizes não definem quem somos, mas sim as histórias que escolhemos contar sobre elas. 🌟",
-   "Às vezes, o silêncio diz mais do que mil palavras, pois é nele que encontramos a verdadeira essência de nós mesmos. 🌿",
-   "A jornada da vida não é medida pelo número de passos que damos, mas sim pelos momentos que nos deixam sem fôlego. 🌄",
-   "A beleza está nos olhos de quem vê, mas a verdadeira beleza reside na capacidade de enxergar além das aparências. 💫",
-   "Não podemos controlar as ondas que nos atingem, mas podemos aprender a surfar sobre elas e encontrar nosso equilíbrio. 🌊",
-   "Às vezes, é preciso se perder para se encontrar verdadeiramente. 🌌",
-   "A gratidão é a chave que abre a porta para a felicidade duradoura. 🙏",
-   "A força não está em nunca cair, mas sim em levantar-se cada vez que caímos. 💪",
-   "O tempo é um presente precioso que devemos gastar com sabedoria, pois é o único recurso que nunca poderemos recuperar. ⏳",
-   "<h5 class='text-center text-info'>Sábias Reflexões para a Jornada da Vida 🌟</h5>",
+   "Em um mundo onde as aparências muitas vezes enganam, Sofia brilha com sua capacidade única de ver além do óbvio. Ela nos lembra que a verdadeira beleza reside no interior de cada ser humano.",   
+   "Para Sofia, cada pessoa é uma obra de arte em constante evolução, Um tesouro a ser descoberto, uma história a ser compartilhada. Ela nos motiva a cultivar empatia e compaixão em nossas interações diárias.",   
+   "Com sua visão inspiradora, aprendemos que a essência de uma pessoa vai além de sua aparência externa, E que ao olharmos com o coração, descobrimos a verdadeira magia da vida. ",
+   "<h5 class='text-center text-info'>Além das Aparências: A Arte de Enxergar com o Coração ❤️</h5>",
 ]
 
 var indice_atual_1 = -1;
