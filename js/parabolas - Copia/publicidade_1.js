2e0bc5aba08fe9aa641910e589ab4eb8901f4e8f
Moises Nunes <moinunes@gmail.com>
2024-03-28 04:09:10 -0300

    reformulando o site

--- js/parabolas - Copia/publicidade_1.js
+++ js/parabolas - Copia/publicidade_1.js
@@ -4,35 +4,38 @@
 function obterPublicidade() {
    
    var frases = [
-      `<a href="https://amzn.to/3Vw8i6h">            
+      `<a href="https://amzn.to/3Vw8i6h"> 
+         <span class="font_0 ">Publicidade</span><br>               
          <img src="/images/publicidade/amazon/caneca_porcelana_1.png" class="img-fluid" alt="Imagem da Caneca de Porcelana">
-         <p class="font_1 text-center m-0">                      
+         <p class="font_1 m-0" id="frase4">                      
             <i>Canecas de Porcelana Personalizadas</i>
         </p> 
       </a>`,
 
-      `<a href="https://amzn.to/3TQCvM3">             
+      `<a href="https://amzn.to/3TQCvM3"> 
+         <span class="font_0 ">Publicidade</span><br>               
          <img src="/images/publicidade/amazon/caneca_porcelana_2.png" class="img-fluid" alt="Imagem da Caneca de Porcelana">
-         <p class="font_1 text-center m-0 ">                      
+         <p class="font_1 m-0">                      
             <i>Canecas de Porcelana Personalizadas</i>
         </p> 
       </a>`,
 
-      `<a href="https://amzn.to/4axiM9z">
+      `<a href="https://amzn.to/4axiM9z"> 
+         <span class="font_0 ">Publicidade</span><br>               
          <img src="/images/publicidade/amazon/cafe_com_Deus_pai.png" class="img-fluid" alt="Café com Deus Pai 2024: Porções Diárias de paz">
-         <p class="font_1 text-center m-0" >                      
+         <p class="font_1 m-0" >                      
             <i>Café com Deus Pai 2024: Porções Diárias de paz</i>
          </p> 
       </a>`,
 
-      `<a href="https://amzn.to/3xc3Mj3">
+      `<a href="https://amzn.to/3xc3Mj3"> 
+         <span class="font_0 ">Publicidade</span><br>               
          <img src="/images/publicidade/amazon/como_fazer_amigos_dale_carnegie.png" class="img-fluid" alt="Como fazer amigos e influenciar pessoas">
-         <p class="font_1 text-center m-0 p-0" >                      
+         <p class="font_1 m-0" >                      
             <i>Como fazer amigos e influenciar pessoas - Dale Carnegie</i>
          </p> 
-       </a>`,
+      </a>`,
 
-       
 
 
 
