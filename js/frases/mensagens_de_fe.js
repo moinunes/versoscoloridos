96e857c6727e15abd345b8f14011ecdb8a611e89
Moises Nunes <moinunes@gmail.com>
2024-04-07 12:47:11 -0300

    atualizacao adsense

---
+++ js/frases/mensagens_de_fe.js
@@ -0,0 +1,281 @@
+mensagem_1 = [ 
+   "<h5 class='text-center text-info'>Mensagens de Fé em Deus: 🌟</h5>",
+   "Ter fé em Deus é a melhor maneira de encontrar motivação. 🙏",
+   "Acredite que algo maravilhoso está prestes a acontecer. ✨",
+   "Sonhe sem medos, viva sem limites e leve a fé dentro do peito. ✨",
+   "A fé é o combustível que alimenta nossos sonhos. 🌟",
+   "Confie em Deus, mesmo quando não entender os seus planos. 🙏",
+   "Você é mais forte do que as suas desculpas. Basta ter fé.🙏",
+   "A fé não é a ausência de dúvida, mas a coragem de seguir em frente apesar dela. 💪",
+   "Quando tudo parece perdido, a fé nos lembra que sempre há uma saída. 🌈",
+   "Deus está sempre presente, mesmo nos momentos mais difíceis. 🌌",
+   "Acredite no poder das pequenas orações, elas podem mover montanhas. 🗻",
+   "Respire fundo, tenha fé e acredite na força e no amor de Deus para seguir em frente. 🌟",
+   "A fé não faz as coisas serem mais fáceis, mas nos dá força para enfrentá-las. ⚡",
+   "Nunca subestime o poder da sua fé. Ela pode mudar tudo. 🌠",
+   "Que a fé em Deus seja sempre a luz que guia os seus passos. 🕊️", 
+]
+
+
+
+var mensagem_2 = [
+   "<h5 class='text-center text-info'>Jornada de Paz:<br> Encontrando Esperança <br> e Amor em Deus 🌟</h5>",
+   "Com Deus no coração, o amor guia nossos passos em direção à paz interior. ❤️",
+   "A fé nos ensina que, mesmo nos momentos mais sombrios, há esperança de dias melhores. 🙏",
+   "O amor de Deus é a luz que ilumina nossos caminhos, trazendo paz aos corações aflitos. ✨",
+   "Em tempos de incerteza, a fé nos sustenta e a esperança nos fortalece, guiando-nos para a paz interior. 🕊️",
+   "Que a fé em Deus nos encha de esperança, nos encontre em amor e nos conduza à paz eterna. 🙏",
+   "No amor de Deus encontramos a verdadeira paz, e na fé encontramos a esperança que nunca falha. ❤️",
+   "Deus nos concede a paz que excede todo entendimento, quando confiamos Nele com fé e esperança. 🙏",
+   "O amor de Deus é a base da nossa fé, a fonte da nossa esperança e o caminho para a verdadeira paz. ❤️",
+   "Nas dificuldades da vida, encontre conforto no amor de Deus, força na sua fé e esperança na promessa de paz. 🌟",
+   "Com fé em Deus e amor no coração, encontramos esperança e paz. 🙏",
+  "Que a paz de Deus nos guarde, fortalecendo nossa fé e esperança. 🕊️",
+  "Com fé em Deus, cultivamos esperança e paz. 🙏❤️",
+  "O amor de Deus nos inspira, a fé nos sustenta e a esperança nos impulsiona. ❤️",
+  "Que a presença de Deus nos preencha de amor, fortaleça nossa fé e renove nossa esperança. 🙏",
+  "A verdadeira paz é encontrada ao entregarmos nossas preocupações a Deus. 🕊️"
+]; 
+
+mensagem_3 = [
+   "<h5 class='text-center text-info'>Confiança e Fé:<br> Guiados pelo Amor de Deus🌟</h5>",
+   "Levante-se com confiança e erga a cabeça com fé! 🌱",
+   "Deixe a fé em Deus guiar seus passos e a confiança te impulsionar para frente! 🌟🙏",
+   "Não deixe o medo te parar, avance com fé, confiando no amor de Deus! ❤️",
+   "Levante-se após cada queda, confiante na fé em Deus! 🌱",
+   "Com a fé em Deus como bússola, você supera qualquer obstáculo! 🌟",
+   "Mantenha a cabeça erguida, pois a fé em Deus é a luz que ilumina seu caminho! 🙏",
+   "Não deixe o medo te parar, avance com fé, confiando no amor de Deus! ❤️",
+   "Semeie confiança e colha os frutos da fé em Deus em cada passo do seu caminho! 🌿",
+   "Com fé em Deus, erga-se a cada queda! 🙏",
+   "Acredite em suas habilidades, caminhe com fé, amparado pelo amor de Deus! 🌈",
+   "Encare desafios com fé e confiança, no amor de Deus! ❤️",
+   "A fé em Deus é o alicerce firme para levantar-se com confiança e caminhar com fé! 🌱",
+   "Deixe que a fé em Deus ilumine seu caminho, guiando-o para frente com confiança! 🌟🙏",
+   "Com a certeza do amor de Deus, não permita que o medo o impeça de avançar com fé! ❤️",
+   "Cada queda é uma oportunidade para fortalecer sua fé em Deus e erguer-se com renovada determinação! 🌱",
+   "Confie na orientação de Deus para superar os desafios, deixando Sua luz guiar seu caminho! ",
+ ];  
+   
+ 
+var mensagem_4 = [   
+   "<h5 class='text-center text-info'>Sussurros de Fé:<br> A Jornada Iluminada pelo Amor de Deus💖</h5>",
+   "No sussurro do vento, a fé ecoava o amor eterno de Deus. 🌀❤️",
+  "Com cada batida do coração, ela sentia a presença de Deus, alimentando sua fé e amor. 💓",
+  "Entre as estrelas do céu noturno, ela encontrou respostas para suas preces, revelando o amor infinito de Deus. ✨",
+  "Em cada suspiro, ela carregava a certeza de que o amor de Deus guiava seus passos na jornada da fé. 🚶‍♀️",
+  "Na calmaria da noite, a fé brilhava como uma estrela, iluminando o caminho para o amor de Deus. 🌠",
+  "Nas páginas da vida, ela escreveu sua história com as tintas da fé, os traços do amor e a mão de Deus como guia. 📖",
+  "Com cada desafio, ela fortalecia sua fé, alimentada pelo amor divino que irradiava de Deus. ❤️",
+  "No silêncio da oração, ela encontrava conforto no abraço amoroso de Deus, renovando sua fé a cada instante. 🙏",
+  "No brilho do amanhecer, ela via a promessa de um novo dia, repleto do amor e da graça de Deus. ❤️",
+  "Na dança da vida, ela seguia os passos da fé, embalada pelo amor incondicional de Deus. 🙏", 
+   "Com um coração corajoso, ela enfrentou cada desafio, encontrando força na esperança que Deus oferecia. ❤️ ",
+   "No peito, a chama da esperança ardia, alimentando sua alma corajosa com a força de Deus. 🔥 ",
+   "Em seu peito corajoso, a esperança florescia como uma flor, nutrida pela força inabalável de Deus. 🌸",
+   "Cada passo, demonstrava coragem, inspirada pela esperança que Deus lhe dava. 👣",
+   "Ela, corajosa, abraçou cada desafio, fortalecida pela esperança que Deus depositava em seu coração. 🙏",
+   "Ergueu-se sob o céu estrelado, encontrando força na esperança guiada por Deus. 🌟",
+   "Enfrentou o desconhecido com coragem, impulsionada pela esperança divina. ❤️🌟",
+   "Sentiu a força de Deus em cada respiração, alimentando a esperança. 🙏",
+   "No peito, a esperança era seu escudo contra a escuridão, fortalecida pela divina força. ❤️",
+   "Caminhou com coragem, impulsionado pela força da esperança. 🚶‍♂️ ",
+ ];
+  
+
+var indice_atual_1 = -1;
+var indice_atual_2 = -1;
+var indice_atual_3 = -1;
+var indice_atual_4 = -1;
+
+$('html, body').animate({
+   scrollTop: 0
+}, 700);
+
+function proxima_mensagem_1() {
+   mensagens = mensagem_1; 
+   indice_atual_1++;
+   if (indice_atual_1 >= mensagens.length) {
+      indice_atual_1 = 0; // Volta para o início do array se atingir o final
+   }
+   document.getElementById("div_mensagem").innerHTML =  mensagens[indice_atual_1];   
+}
+
+
+function proxima_mensagem_2() {
+   mensagens = mensagem_2; 
+   indice_atual_2++; 
+   if (indice_atual_2 >= mensagens.length) {
+      indice_atual_2 = 0; // Volta para o início do array se atingir o final
+   }
+   document.getElementById("div_mensagem").innerHTML =  mensagens[indice_atual_2];   
+}
+
+function proxima_mensagem_3() {
+   mensagens = mensagem_3; 
+   indice_atual_3++; 
+   if (indice_atual_3 >= mensagens.length) {
+      indice_atual_3 = 0; 
+   }
+   document.getElementById("div_mensagem").innerHTML =  mensagens[indice_atual_3];   
+}
+
+function proxima_mensagem_4() {
+   mensagens = mensagem_4; 
+   indice_atual_4++; 
+   if (indice_atual_4 >= mensagens.length) {
+      indice_atual_4 = 0; 
+   }
+   document.getElementById("div_mensagem").innerHTML =  mensagens[indice_atual_4];   
+}
+
+var modo = new URLSearchParams(window.location.search).get('modo');
+
+function avancarParaProximaMensagem() {   
+   switch (modo) {
+       case '1':
+             proxima_mensagem_1();
+             break;
+       case '2': 
+             proxima_mensagem_2();
+             break;
+       case '3':
+             proxima_mensagem_3();
+             break;
+       case '4':
+             proxima_mensagem_4();
+             break;
+       default:
+             console.error('Número de mensagem inválido:', modo);
+   }
+   obter_frases(); // preenche: div_frase_1,div_frase_2,div_frase_3 e div_frase_4
+} 
+
+// Selecione o link correspondente ao modo atual e adicione a classe btn-primary
+if (modo === '1') {
+    document.querySelector('a[href="/frases/frases_de_fe/mensagens_de_fe.html?modo=1"]').classList.add('btn-primary'); 
+} else if (modo === '2') {
+    document.querySelector('a[href="/frases/frases_de_fe/mensagens_de_fe.html?modo=2"]').classList.add('btn-primary');
+} else if (modo === '3') {
+    document.querySelector('a[href="/frases/frases_de_fe/mensagens_de_fe.html?modo=3"]').classList.add('btn-primary');
+} else if (modo === '4') {
+    document.querySelector('a[href="/frases/frases_de_fe/mensagens_de_fe.html?modo=4"]').classList.add('btn-primary');
+}
+
+
+/*-------------------------------------------------------------
+* obter a frase de forma cíclica
+*-------------------------------------------------------------*/ 
+var frases_1 = [  
+   "Nunca subestime o poder da sua fé para superar qualquer desafio. 🌈💪",
+   "Que a sua fé seja maior do que seus medos e suas dúvidas. 🙌😌",
+   "Em meio às tempestades da vida, mantenha a fé como seu guarda-chuva. ☔️🌧️",
+   "Tenha fé nas reviravoltas da vida, pois até mesmo as noites mais escuras têm um amanhecer. 🌅🙏",
+   "Acredite no extraordinário, pois a fé é a chave para abrir as portas do impossível. 🔑✨",
+   "Deixe sua fé ser maior do que seus problemas e você encontrará soluções além da imaginação. 💡🙏",
+     "A cada desafio, fortalecia sua fé com o amor que irradiava de Deus. 💪❤️",
+   "Na quietude da oração, encontrava consolo no abraço amoroso do divino. 🙏💖",
+   "Cada batida do coração era uma lembrança da presença amorosa de Deus. 💓🙏",
+   "No sussurro do vento, ouvia a ecoar a fé que a sustentava, guiada pelo amor eterno de Deus. 🌀❤️",
+   "Entre as estrelas do céu noturno, encontrou respostas para suas preces, revelando o amor infinito de Deus. ✨🙏",
+   "No calor do sol poente, sentia-se abraçada pelo amor de Deus, renovando sua esperança a cada dia. ☀️❤️",
+   "Nas margens do rio da vida, encontrou a coragem para enfrentar as correntezas, guiada pela mão amorosa de Deus. 🌊🙏",
+   "No abraço da natureza, sentiu a presença reconfortante de Deus, alimentando sua fé e esperança. 🌿🙏",
+];
+
+var frases_2 = [
+   "Em tempos de dificuldade, a fé é o farol que ilumina nosso caminho. 🕯️🙏",
+   "Acredite que cada desafio é uma oportunidade de crescimento e aprendizado. 🌱📈",
+   "Nunca foi sorte, sempre foi Deus! A fé em Deus renova toda a criatura. 🍀🙌",
+   "Esse é o segredo da vida: fé! A fé manifesta a presença de Deus em nossas vidas. 🔑🌟",
+   "Deus é a bússola que norteia a minha vida! 🧭🙏",
+   "Porque vivemos por fé, e não pelo que vemos. 🌟👀",
+   "Cada desafio era uma oportunidade para fortalecer sua fé e se aproximar do amor divino. 💪❤️",
+   "Na simplicidade do cotidiano, encontrava a presença amorosa de Deus em cada detalhe. 🌼🙏",
+   "Com os olhos voltados para o céu, sentia a paz que vem da certeza do amor de Deus. 🌌❤️",
+   "Em meio às tempestades da vida, encontrou abrigo seguro no amor inabalável de Deus. ⛈️🙏",
+   "Cada passo era um testemunho de sua fé inabalável e do amor constante de Deus. 👣❤️",
+   "Na solitude da alma, encontrou conforto na companhia silenciosa de Deus. 🕊️🙏",
+ 
+]; 
+
+var frases_3 = [
+   "Acredite em si mesmo e em seu potencial, pois a fé é o primeiro passo em direção ao sucesso. 💪🌟",
+   "A fé move montanhas e transforma sonhos em realidade. 🏔️✨",
+   "Em meio aos desafios, encontrou força na fé e esperança que Deus plantou em seu coração. 💪🌱",
+   "Cada amanhecer era um lembrete do amor renovado de Deus, alimentando sua alma com esperança. 🌅❤️",
+   "Na simplicidade do presente, encontrou a presença divina, guiando seus passos com amor e graça. 🙏✨",
+   "Com cada respiração, sentia a bênção do amor de Deus, enchendo seu coração de paz e serenidade. 💨❤️",
+   "No brilho dos olhos de uma criança, viu o reflexo do amor divino, puro e incondicional. 👶❤️",
+   "Nas asas do vento, sentiu-se livre para voar, confiante no amor e cuidado de Deus. 🕊️❤️",
+   "Com cada batida do coração, ecoava a canção do amor eterno de Deus, que nunca se esgota. 💓🎶",
+   "Através das lágrimas, via o reflexo do amor divino, lembrando que Deus nunca a abandonaria. 😢❤️",
+   "Na imensidão do universo, sentiu-se pequena diante do amor infinito de Deus que a envolvia. 🌌❤️",
+   "Em cada sorriso, sentia a alegria que vem da certeza de ser amada por Deus. 😊❤️",
+   "Encare cada novo dia com coragem e fé no coração. 💪🙏",
+   "Acredite em si mesmo e deixe que sua fé o guie para alcançar seus sonhos. ✨🌟",
+ ]; 
+
+ var frases_4 = [
+   "Nas páginas da vida, encontrou as pegadas de Deus, guiando-a com amor e proteção. 📖👣",
+   "No calor do abraço de um amigo, sentiu o amor de Deus se manifestar, confortando sua alma. 🤗❤️",
+   "A cada pôr do sol, lembrava-se da fidelidade do amor de Deus, que nunca falha. 🌇❤️",   
+   "Tenha fé e espere sem cessar. 🙏⏳",
+   "Coloque fé em tudo que fizer, pois para Deus não há limites. 💪🙌",
+   "Nos momentos de escuridão, encontrou a luz da esperança brilhando através da fé em Deus. 🕯️🙏",
+   "Nunca desista, pois a fé transforma obstáculos em oportunidades. 🚀💪",
+   "Que a sua fé seja inabalável, como uma rocha que enfrenta as ondas do mar. 🌊🙏",
+   "No brilho da aurora, encontrou a promessa de um novo dia, repleto do amor e da graça divina. 🌅❤️",
+   "Nas páginas da vida, escreveu sua história com a tinta da fé, os traços do amor e a orientação de Deus. 📖🙏",
+ ]
+
+function obterFrase_1() {
+   let frases = frases_1.concat(frases_2, frases_3, frases_4);
+   let frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+   let segundoAtual = new Date().getSeconds();
+   let indiceFrase = segundoAtual % frases.length;
+   let fraseCorrespondente = frases[indiceFrase] || frase_padrao;   
+   return fraseCorrespondente;
+}
+function obterFrase_2() {
+   let frases = frases_2.concat(frases_3, frases_4, frases_1);
+   let frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+   let segundoAtual = new Date().getSeconds();
+   let indiceFrase = segundoAtual % frases.length;
+   let fraseCorrespondente = frases[indiceFrase] || frase_padrao;   
+   return fraseCorrespondente;
+}
+function obterFrase_3() {
+   let frases = frases_3.concat(frases_4, frases_1, frases_2);
+   let frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+   let segundoAtual = new Date().getSeconds();
+   let indiceFrase = segundoAtual % frases.length;
+   let fraseCorrespondente = frases[indiceFrase] || frase_padrao;   
+   return fraseCorrespondente;
+}
+function obterFrase_4() {
+   let frases = frases_4.concat(frases_1, frases_2, frases_3);
+   let frase_padrao = "Sua autenticidade é sua beleza.<br> Abrace quem você é, pois você é único e especial.";
+   let segundoAtual = new Date().getSeconds();
+   let indiceFrase = segundoAtual % frases.length;
+   let fraseCorrespondente = frases[indiceFrase] || frase_padrao;   
+   return fraseCorrespondente;
+}
+
+function obter_frases() {
+   var divMens = document.getElementById("div_frase_1"); 
+   var frase = obterFrase_1(); 
+   divMens.innerHTML = frase; 
+   //...
+   var divMens = document.getElementById("div_frase_2"); 
+   var frase = obterFrase_2(); 
+   divMens.innerHTML = frase; 
+   //...
+   var divMens = document.getElementById("div_frase_3"); 
+   var frase = obterFrase_3(); 
+   divMens.innerHTML = frase; 
+   //...
+   var divMens = document.getElementById("div_frase_4"); 
+   var frase = obterFrase_4(); 
+   divMens.innerHTML = frase; 
+}
